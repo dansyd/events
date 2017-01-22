892fa18de1ed348d01b848c48e745fb4e085d920
Daniele Peviani <daniele.peviani@gmail.com>
2017-01-22 22:33:48 +1100

    Created user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id #going back to 
+      session[:user_id] = @user.id #going back to
       redirect_to root_path
     else
       render :new
     end
   end
 
+  def edit
+    # check_if_logged_in
+    @user = @current_user
+  end
+
+  def update
+    user = @current_user
+    user.update user_params
+    redirect_to edit_user_path(user.id)
+  end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :dob, :sex)
   end
 end
