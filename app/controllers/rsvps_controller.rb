ea9f920b982dac4cea3e37472590d71dcc0968bf
Daniele Peviani <daniele.peviani@gmail.com>
2017-01-25 12:32:58 +1100

    Fixed attending status, changed var name conflict -> from action to perform_action

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -2,12 +2,12 @@ class RsvpsController < ApplicationController
 
   def create
     rsvp = @current_user.rsvps.find_by event_id: params[:event_id]
-    if params[:action] == 'destroy'
+    if params[:perform_action] == 'destroy'
       rsvp.destroy
-      redirect_to :back
+      return redirect_to :back
     end
 
-    if params[:action] == 'destroy'
+    if params[:perform_action] == 'create'
       rsvp = Rsvp.new
       rsvp.user_id = @current_user.id
       rsvp.event_id = params[:event_id]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def create_rsvp_options_link(label, status, event, action)
-    link_to label, event_rsvps_path(event, :status => status, :action => action), :method => 'post'
+  def create_rsvp_options_link(label, status, event, perform_action)
+    link_to label, event_rsvps_path(event, :status => status, :perform_action => perform_action), :method => 'post'
   end
 
 end
