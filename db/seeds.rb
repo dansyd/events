64e43f308f71d15ae7d79884f094a2908968c266
Daniele Peviani <daniele.peviani@gmail.com>
2017-01-30 10:58:30 +1100

    Changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,15 @@ u2 = User.create :email => 'mark@gmail.com', :dob => '1973/03/17', :sex => 'M', 
 u3 = User.create :email => 'emily@gmail.com', :dob => '1985/02/05', :sex => 'F', :password => 'chicken', :password_confirmation => 'chicken'
 
 Event.destroy_all
-e1 = Event.create :name => 'Night Markets', :location => 'Carriageworks 245 Wilson Street, Eveleigh', :start_datetime => '2017-01-29T17:00:00', :end_datetime => '2017-01-29T22:00:00', :description => 'Set within the Carriageworks precinct and inspired by the streets of Harajuku, Hongdae, and AnFu Lu, the Night Market will immerse you in the food, pop culture and sounds of contemporary Asia.', :image => 'nightmarkets_gigpxb'
+e1 = Event.create :name => 'Night Markets', :location => 'Carriageworks 245 Wilson Street, Eveleigh', :start_datetime => '2017-02-24T17:00:00', :end_datetime => '2017-02-24T22:00:00', :description => 'Set within the Carriageworks precinct and inspired by the streets of Harajuku, Hongdae, and AnFu Lu, the Night Market will immerse you in the food, pop culture and sounds of contemporary Asia.', :image => 'nightmarkets_gigpxb'
 
-e2 = Event.create :name => 'Workshop: Native bee keeping with Tim Heard', :location => '25-27 Lambert St, Camperdown NSW 2050, Australia', :start_datetime => '2017-02-04T09:30:00', :end_datetime => '2017-02-04T15:30:00', :description => 'Tim Heard, author of The Australian Native Bee Book, will facilitate the popular native bee keeping course. The day will be 6 hours of engaging and interactive learning.
+e2 = Event.create :name => 'Workshop: Native bee keeping with Tim Heard', :location => '25-27 Lambert St, Camperdown NSW 2050, Australia', :start_datetime => '2017-03-19T09:30:00', :end_datetime => '2017-03-19T15:30:00', :description => 'Tim Heard, author of The Australian Native Bee Book, will facilitate the popular native bee keeping course. The day will be 6 hours of engaging and interactive learning.
 
 During the day, the topics of bee biology, importance of bees in natural systems and agriculture, rescuing bees threatened in the wild, and keeping stingless bee hives for pets, pollination and for the sought after sugarbag honey are covered.
 
 The day will alternate between slide shows and practical sessions.', :image => 'bee-keeping_qrsotl'
 
-e3 = Event.create :name => 'Free heartfulness meditation', :location => '17 Oxford St, Paddington NSW 2021, Australia', :start_datetime => '2017-01-30T18:00:00', :end_datetime => '2017-01-30T19:00:00', :description => 'Heartfulness enables you to explore meditation more deeply.
+e3 = Event.create :name => 'Free heartfulness meditation', :location => '17 Oxford St, Paddington NSW 2021, Australia', :start_datetime => '2017-03-30T18:00:00', :end_datetime => '2017-03-30T19:00:00', :description => 'Heartfulness enables you to explore meditation more deeply.
 
 It allows you to experience the benefits that come from practising the last four steps of Patanjali’s ashtanga yoga – Prathyahara, Dharana, Dhyana (meditation) and Samadhi.
 
